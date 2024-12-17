c0198787b403e8c74f473b6b0da4a80c8d075932
Xav <bonjour@xaviers.sh>
2024-12-17 13:27:56 -0500

    optimize font loading using astro-font

---
+++ src/constants/fonts.ts
@@ -0,0 +1,9 @@
+export const Inconsolata = {
+    src: [],
+    name: 'Inconsolata',
+    googleFontsURL: 'https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900',
+    preload: true,
+    display: 'swap',
+    selector: 'body',
+    fallback: 'monospace'
+}
