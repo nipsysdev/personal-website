ef28a53380421c176427eb314a9cfc1578be2edf
Xav <bonjour@xaviers.sh>
2024-12-20 16:07:26 -0500

    fix font definition typing error

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,9 +1,10 @@
 export const Inconsolata = {
-    src: [],
-    name: 'Inconsolata',
-    googleFontsURL: 'https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900',
-    preload: true,
-    display: 'swap',
-    selector: 'body',
-    fallback: 'monospace'
-}
+  src: [],
+  name: "Inconsolata",
+  googleFontsURL:
+    "https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900",
+  preload: true,
+  display: "swap",
+  selector: "body",
+  fallback: "monospace" as const,
+};
