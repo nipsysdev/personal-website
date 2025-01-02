f5b45f75f6d1ed334db9a95503960a1991fe6ecf
Xav <bonjour@xaviers.sh>
2025-01-01 19:51:27 -0500

    making use of Astro.currentLocale

--- src/i18n/I18nWrapper.tsx
+++ src/i18n/I18nWrapper.tsx
@@ -1,15 +1,15 @@
 import { type JSX, useEffect } from "react";
 import { CurrentLang } from "../stores/coreStore.ts";
-import type { Lang } from "../constants/lang.ts";
+import { Lang } from "../constants/lang.ts";
 
 interface Props {
-  lang: Lang;
+  lang: string | undefined;
   children: JSX.Element | null;
 }
 
 export default function I18nWrapper(props: Props) {
   useEffect(() => {
-    CurrentLang.set(props.lang);
+    CurrentLang.set((props.lang as Lang | undefined) ?? Lang.En);
   }, [props.lang]);
   return props.children;
 }
