c19c3a4a2f8249f45a8b11fd36adbbfa64bdbc3a
Xav <bonjour@xaviers.sh>
2025-01-01 20:00:36 -0500

    set dayjs locale in both astro and react

--- src/i18n/I18nWrapper.tsx
+++ src/i18n/I18nWrapper.tsx
@@ -1,6 +1,9 @@
 import { type JSX, useEffect } from "react";
 import { CurrentLang } from "../stores/coreStore.ts";
 import { Lang } from "../constants/lang.ts";
+import dayjs from "dayjs";
+import "dayjs/locale/en";
+import "dayjs/locale/fr";
 
 interface Props {
   lang: string | undefined;
@@ -9,7 +12,9 @@ interface Props {
 
 export default function I18nWrapper(props: Props) {
   useEffect(() => {
-    CurrentLang.set((props.lang as Lang | undefined) ?? Lang.En);
+    const currentLang = (props.lang as Lang | undefined) ?? Lang.En;
+    CurrentLang.set(currentLang);
+    dayjs.locale(currentLang);
   }, [props.lang]);
   return props.children;
 }
