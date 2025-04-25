9db6fdf7c6b1ddb962ccb348b994f3246b0e8827
Xavier Saliniere <bonjour@xaviers.sh>
2025-04-25 16:41:16 -0400

    chore(format): format files using prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,16 @@ import eslint from "@eslint/js";
 import tsEslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 import jsxA11y from "eslint-plugin-jsx-a11y";
-import tseslint from 'typescript-eslint'
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['.astro', 'dist'] },
+  { ignores: [".astro", "dist"] },
   {
-    extends: [eslint.configs.recommended,
-        ...tsEslint.configs.recommended,
-        ...eslintPluginAstro.configs.recommended,
-        jsxA11y["flatConfigs"].recommended],
+    extends: [
+      eslint.configs.recommended,
+      ...tsEslint.configs.recommended,
+      ...eslintPluginAstro.configs.recommended,
+      jsxA11y["flatConfigs"].recommended,
+    ],
   },
-)
+);
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		colors: {
-			olivedrab: '#839818',
-			darkslategray: '#1c3848',
-			darkerslategray: '#0d1c25',
-			darkgray: '#B1B2B5',
-			steelblue: '#378fd2',
-			firebrick: '#d02c26',
-			lightseagreen: '#2ca299',
-			darkgoldenrod: '#bd920a',
-		}
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      olivedrab: "#839818",
+      darkslategray: "#1c3848",
+      darkerslategray: "#0d1c25",
+      darkgray: "#B1B2B5",
+      steelblue: "#378fd2",
+      firebrick: "#d02c26",
+      lightseagreen: "#2ca299",
+      darkgoldenrod: "#bd920a",
+    },
+  },
+  plugins: [],
+};
